eea7b4b04f934e0263c6d2c8e34de31de384757d
sashapoliti <sas.politi@gmail.com>
2024-05-09 10:55:30 +0200

    fix styles bugs

--- index.php
+++ index.php
@@ -6,16 +6,15 @@ $foods = Prod::create('/foods_db.json', 'Foods');
 /* $games = Prod::create('/games', 'Games'); */
 ?>
 
-<main class="container">
+<main>
 
     <!-- food section -->
-    <section>
+    <section class="container">
         <h2>Cibo</h2>
-        <div class="row">
-            <div class="col-12 col-md-4 col-lg-3">
-            <?php foreach($foods as $food) : ?>
-                <div class="card d-flex">
-                    <img src="<?= $food->img ?>" class="card-img-top" alt="<?= $food-> name ?>">
+        <div class="row"><?php foreach ($foods as $food): ?>
+                <div class="col-4">
+                    <div class="card">
+                        <img src="<?= $food->img ?>" class="card-img-top" alt="<?= $food->name ?>">
                         <div class="card-body">
                             <h5 class="card-title"><?= $food->name ?></h5>
                             <p class="card-text"><?= $food->description ?></p>
@@ -23,31 +22,24 @@ $foods = Prod::create('/foods_db.json', 'Foods');
                             <p class="card-text"><?php $food->id ?></p>
                             <a href="#" class="btn btn-primary">Go somewhere</a>
                         </div>
-                        <?php endforeach; ?>
-                </div>
-            </div>
+                    </div>
+                </div><?php endforeach; ?>
         </div>
     </section>
 
     <!-- toys section -->
     <section>
-        <h2>gatti</h2>
+        <h2>Giochi</h2>
         <div class="row">
             <div class="col-12 col-md-4 col-lg-3">
 
             </div>
         </div>
     </section>
-    <section>
-        <h2>pesci</h2>
-        <div class="row">
-            <div class="col-12 col-md-4 col-lg-3">
 
-            </div>
-        </div>
-    </section>
+    <!-- accessories section -->
     <section>
-        <h2>uccelli</h2>
+        <h2>Accessori</h2>
         <div class="row">
             <div class="col-12 col-md-4 col-lg-3">
 
